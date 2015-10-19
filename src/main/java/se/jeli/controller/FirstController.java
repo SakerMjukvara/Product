213e2b426006f46951257f29bfb78cd88f36367a
hejenstedt <hejenstedt@users.noreply.github.com>
2015-10-19 10:24:37 +0200

    Några blandade sidor, har länkat in epay. Går inte att få tillbaka någon form av confirmation innan den ligger på en riktig https eller ftp Url men det går iaf att testa betalningen.

--- src/main/java/se/jeli/controller/FirstController.java
+++ src/main/java/se/jeli/controller/FirstController.java
@@ -1,5 +1,14 @@
 package se.jeli.controller;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,19 +28,53 @@ public class FirstController {
 
 	}
 
-	@RequestMapping(value="/login", method= RequestMethod.POST)
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(String name, String pw, Model model) {
 
 		boolean authorized = validate.isAuthorized(name, pw);
-		
-		model.addAttribute("loginResult", authorized);
 
-		return "result";
+		if (authorized) {
+			model.addAttribute("userName", name);
+			return "result";
+		}
+
+		model.addAttribute("loginStatus", "Login misslyckades");
+		return home();
+
 	}
 
-	@RequestMapping("/index")
+	@RequestMapping(value = "/buy", method = RequestMethod.POST)
+	public String buy(int number, Model model, HttpServletRequest httpSession) {
+		Enumeration<String> attributeNames = httpSession.getAttributeNames();
+		Map<String, Object> attributes = new HashMap<>();
+		while (attributeNames.hasMoreElements()) {
+
+			String nextElement = attributeNames.nextElement();
+			Object attribute = httpSession.getAttribute(nextElement);
+			attributes.put(nextElement, attribute);
+		}
+
+		model.addAttribute("attributes", attributes);
+		model.addAttribute("number", number);
+
+		return "order";
+	}
+
+	@RequestMapping(value = "confirmation")
+	public String confirmation(HttpSession httpSession){
+		
+		
+		return "confirmation";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+		return home();
+	}
+
+	@RequestMapping("/")
 	public String home() {
-		return "login";
+		return "index";
 	}
 
 }
